520923c6a2a360c701d1970cdecadfac80d3cab3
GiangGiang1105 <nnhgiang.17it1@vku.udn.vn>
2021-09-10 23:42:58 +0700

    update date converter

--- ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/User.kt
+++ ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/User.kt
@@ -3,11 +3,14 @@ package com.example.connectorlibrary.enitity
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.connectorlibrary.base.DateConverter
 import kotlinx.android.parcel.Parcelize
-import java.sql.Date
+import java.util.Date
 
 @Entity(tableName = "user")
 @Parcelize
+@TypeConverters(DateConverter::class)
 data class User(
     @PrimaryKey(autoGenerate = true)
     val user_id: Int = 0,
@@ -18,5 +21,5 @@ data class User(
     val gender_id: Int = 0,
     val email: String = "",
     val address: String = "",
-    val active_id : Int = 0
+    val active_id: Int = 0
 ) : Parcelable
