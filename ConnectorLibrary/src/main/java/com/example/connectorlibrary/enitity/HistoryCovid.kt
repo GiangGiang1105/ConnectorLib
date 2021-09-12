a9c0cad961a7dc33d8562e2ad72a272855e3e537
GiangGiang1105 <nnhgiang.17it1@vku.udn.vn>
2021-09-12 14:26:46 +0700

    update version 0.2.8

--- ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/HistoryCovid.kt
+++ ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/HistoryCovid.kt
@@ -1,16 +1,22 @@
 package com.example.connectorlibrary.enitity
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.connectorlibrary.base.DateConverter
+import com.example.connectorlibrary.base.ListConverter
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@TypeConverters(ListConverter::class)
 @Entity(tableName = "history_covid")
 data class HistoryCovid(
     @PrimaryKey(autoGenerate = true)
-    val history_id : Int = 0,
-    val area : String = "all",
-    val date: Long = 0,
-    val cases: Long = 0
-): Parcelable
+    val history_id: Int = 0,
+    val area: String = "all",
+    val status: Int = 0,
+    @ColumnInfo(name = "list_people_in_day")
+    val listPeopleInDay: List<PeopleInDay>
+) : Parcelable
