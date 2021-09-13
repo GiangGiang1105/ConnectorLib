bf5d5ad1ff5bc8d1a9a4b6dfd4faa8173a7adf90
GiangGiang1105 <nnhgiang.17it1@vku.udn.vn>
2021-09-13 14:45:38 +0700

    update version 0.3.1

--- ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/Health.kt
+++ ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/Health.kt
@@ -4,18 +4,17 @@ import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
-import com.example.connectorlibrary.base.DateConverter
 import com.example.connectorlibrary.base.ListSymptomConverter
 import kotlinx.android.parcel.Parcelize
 import java.sql.Date
 
 @Entity(tableName = "health")
 @Parcelize
-@TypeConverters(DateConverter::class, ListSymptomConverter::class)
+@TypeConverters(ListSymptomConverter::class)
 data class Health(
     @PrimaryKey(autoGenerate = true)
     val health_id: Int = 0,
-    val symptom_id: List<Int>,
+    val list_symptom_id: List<Int>,
     val user_id: Int = 0,
-    val declare_time: Date
+    val declare_time: Long = 0
 ) : Parcelable
--- ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/User.kt
+++ ConnectorLibrary/src/main/java/com/example/connectorlibrary/enitity/User.kt
@@ -4,20 +4,18 @@ import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
-import com.example.connectorlibrary.base.DateConverter
 import kotlinx.android.parcel.Parcelize
 import java.util.Date
 
 @Entity(tableName = "user")
 @Parcelize
-@TypeConverters(DateConverter::class)
 data class User(
     @PrimaryKey(autoGenerate = true)
     val user_id: Int = 0,
     val name: String = "",
     val phone_number: String = "",
     val passport_number: String = "",
-    val birthday: Date,
+    val birthday: Long = 0,
     val gender_id: Int = 0,
     val email: String = "",
     val address: String = "",
